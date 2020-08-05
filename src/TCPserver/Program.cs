6ddb6ddc6135f2b99758eb1b16168d62dd8cf7f2
Oleg <oreshek1111@yandex.ru>
2020-08-05 18:55:38 +0300

    Init chat

---
+++ src/TCPserver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TCPserver
+{
+    class Program
+    {
+        static ServerObject server; // сервер
+        static Thread listenThread; // потока для прослушивания
+        static void Main(string[] args)
+        {
+            try
+            {
+                server = new ServerObject();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+
+
+
+}
